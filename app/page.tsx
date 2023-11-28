0aefc93bd1e392741db22300fb25cb9b1a0564f4
Denis Fadeev <denis@fadeev.org>
2023-11-28 17:12:59 +0400

    fix: build errors

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function IndexPage() {
       <div className="grid sm:grid-cols-3 gap-x-10">
         <div className="sm:col-span-2 overflow-x-scroll">
           <div className="flex items-center justify-start gap-2 mt-12 mb-6">
-            <h1 className="leading-10 text-2xl font-bold leading-tight tracking-tight pl-4">
+            <h1 className="leading-10 text-2xl font-bold tracking-tight pl-4">
               Portfolio
             </h1>
             <Button size="icon" variant="ghost" onClick={refreshBalances}>
